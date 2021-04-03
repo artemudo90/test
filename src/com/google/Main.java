a79e3747537712a8b0f41bd991ecdd95399be0ba
artemudo90 <artemudo90@gmail.com>
2021-04-03 23:33:01 +0300

    Connect to server

--- src/com/google/Main.java
+++ src/com/google/Main.java
@@ -3,5 +3,10 @@ package com.google;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        connect();
+    }
+
+    public static void connect() {
+        System.out.println("connected to server");
     }
 }
